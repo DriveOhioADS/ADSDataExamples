9e816b895630a6ee1bc167902552d6ccf659a1e1
Jay Wilhelm <jwilhelm@ohio.edu>
2024-05-07 09:22:13 -0400

    added single topic pull

--- example_pulltopic.py
+++ example_pulltopic.py
@@ -3,7 +3,7 @@ from ADSDynamo import ADSDynamo
 datainterface = ADSDynamo()
 response = datainterface.GrabCyberDataByTopic(
     groupID = '90101c36-a621-11ee-88ec-eb6a8d5269b4', 
-    topicName = '/apollo/sensor/gnss/best_pose',
+    topicName = '/apollo/sensor/gnss/odometry',#'/apollo/sensor/gnss/best_pose',
     limit = 1)
 
 print(response)
